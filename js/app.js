fc7543e0a8c8c041ac3cf4de8c1b7d69b14dcf8c
Amy Cohen <anbcohen@gmail.com>
2018-03-06 14:12:05 -0800

    added the 2nd question and if-statement

--- js/app.js
+++ js/app.js
@@ -5,18 +5,27 @@ var dogOwnership = prompt('Do you think I have any dogs?');
 
 if (dogOwnership.toLowerCase() === 'yes') {
   alert('Sadly, I do not have any dogs.');
-  console.log('This is the incorrect answer.');
 } else if (dogOwnership.toLowerCase() === 'no') {
   alert('While you are correct, it still makes me sad. I want a dog!');
-  console.log('This is the correct answer.');
 } else {
   alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
-  console.log('This should not run because I\'m using yes and no answers.');
 }
 
-console.log('QUESTION: Do you think I have any dogs?  Your answer: ' + dogOwnership);
+console.log('QUESTION: Do you think I have any dogs?  YOUR ANSWER: ' + dogOwnership.toLowerCase());
 
-// var collegeDegree = prompt('Do you think I have a college degree?');
+
+var collegeDegree = prompt('Do you think I have a college degree?');
+
+if (collegeDegree.toLowerCase() === 'yes') {
+  alert('Yes! You are correct. I have a bachelor\'s in Legal Office Administration.');
+} else if (collegeDegree.toLowerCase() === 'no') {
+  alert('You are incorrect. I earned a bachelor\'s in Legal Office Administration.');
+} else {
+  alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
+}
+console.log('QUESTION: Do you think I have a college degree?  YOUR ANSWER: ' + collegeDegree.toLowerCase());
+
+
 // var comuteToClass = prompt('Do you think I drive to class?');
 // var likeSewing = prompt('Do you think I like to sew?');
 // var likeCamping = prompt('Do you think I like camping?');
