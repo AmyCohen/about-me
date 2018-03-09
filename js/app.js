4efde799b7cfb0c580ef287dec7a246de8b85c3b
Amy Cohen <anbcohen@gmail.com>
2018-03-08 16:22:39 -0800

    added cleaner grammar and modified the console.logs to have line breaks for easier reading.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var userName = prompt('Thank you for visiting. What name would you like to be kn
 function dogOwnership() {
 //first question var and if statement
   var dogOwnership = prompt(userName + ', do you think I have any dogs? Please answer with yes or no.');
+
   if (dogOwnership.toLowerCase() === 'yes') {
     alert('Sadly, I do not have any dogs.');
   } else if (dogOwnership.toLowerCase() === 'no') {
@@ -17,13 +18,15 @@ function dogOwnership() {
   } else {
     alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
   }
-  console.log('QUESTION: Do you think I have any dogs?   ' + userName + '\'s Answer: ' + dogOwnership.toLowerCase());
+
+  console.log('Question #1:\nI asked, "Do you think I have any dogs?"\n' + userName + ' answered with ' + dogOwnership.toLowerCase());
 }
 dogOwnership();
 
 function collegeDegree() {
 //second question var and if statement
   var collegeDegree = prompt(userName + ', do you think I have a college degree? Please answer with yes or no.');
+
   if (collegeDegree.toLowerCase() === 'yes') {
     alert('Yes! You are correct. I have a bachelor\'s in Legal Office Administration.');
     howManyCorrect ++;
@@ -33,9 +36,11 @@ function collegeDegree() {
     alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
   }
 
-  console.log('QUESTION: Do you think I have a college degree?   ' + userName + '\'s Answer: ' + collegeDegree.toLowerCase());
+  console.log('Question #2:\nI asked, "Do you think I have a college degree?"\n' + userName + ' answered with ' + collegeDegree.toLowerCase());
 }
+
 collegeDegree();
+
 function commuteClass() {
 //third question var and if statement
   var commuteToClass = prompt(userName + ', do you think I drive to class?  Please answer with yes or no.');
@@ -49,9 +54,11 @@ function commuteClass() {
     alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
   }
 
-  console.log('QUESTION: Do you think I drive to class?   ' + userName + '\'s Answer: ' + commuteToClass.toLowerCase());
+  console.log('Question #3:\nI asked, "Do you think I drive to class?"\n' + userName + ' answered with ' + commuteToClass.toLowerCase());
 }
+
 commuteClass();
+
 function likeSewing() {
 //fourth question var and if statement
   var likeSewing = prompt(userName + ', do you think I like to sew? Please answer with yes or no.');
@@ -64,10 +71,13 @@ function likeSewing() {
   } else {
     alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
   }
+  // console.log('Question #1:\n I asked "Do you think I have any dogs?"\n   ' + userName + ' answered with ' + dogOwnership.toLowerCase());
 
-  console.log('QUESTION: Do you think I like to sew?   ' + userName + '\'s Answer: ' + likeSewing.toLowerCase());
+  console.log('Question #4:\nI asked, "Do you think I like to sew?"\n' + userName + ' answered with ' + likeSewing.toLowerCase());
 }
+
 likeSewing();
+
 function likeCamping() {
 //fifth question var and if statement
   var likeCamping = prompt(userName + ', do you think I like camping? Please answer with yes or no.');
@@ -81,48 +91,55 @@ function likeCamping() {
     alert('You have entered an invalid answer. Please limit your responses to "yes" or "no".');
   }
 
-  console.log('QUESTION: Do you think I like camping?   ' + userName + '\'s Answer: ' + likeCamping.toLowerCase());
+  console.log('Question #5:\nI asked, "Do you think I like camping?"\n' + userName + ' answered with ' + likeCamping.toLowerCase());
 }
+
 likeCamping();
 
 //sixth question in a while loop with a nested if statement
 function howManyBooks() {
   var bookChance = 4;
   var qtyBooksOwned = 3968;
+
   while (bookChance > 0) {
-    var howManyBooks = parseInt(prompt('How many physical (not digital) books do you think I own? Please answer with a whole number.'));
+    var howManyBooks = parseInt(prompt('How many physical (not digital) books do you think I own? Please answer with a whole number. You have ' + bookChance + ' tries to get it correct.'));
+
     if(howManyBooks === qtyBooksOwned){
-      alert('How did you know?! I am an avid reader and perfer my books in hard copy due to current legislation...also I cannot get over the wonderful feel and smell of books.');
+      alert('CORRECT! How did you know?! I am an avid reader and perfer my books in hard copy due to current legislation...also I cannot get over the wonderful feel and smell of books.');
       console.log(userName + ' answered ' + howManyBooks + ' on chance number ' + bookChance);
       howManyCorrect ++;
       break;
     } else if (howManyBooks <= 2000) {
-      alert('That number is waaaaay too low.');
+      alert('That number is waaaaay too low. Please guess again.');
     } else if (howManyBooks >= 2000 && howManyBooks < qtyBooksOwned) {
-      alert('That is a lot less than I have but you\'re thinking in the right direction.');
+      alert('Nope. That is a lot less than I have but you\'re thinking in the right direction.');
     } else if (howManyBooks > qtyBooksOwned) {
       alert('While that would be totally awesome, sadly it is not true. I have less than you have guessed.');
     } else {
       alert('Hmmm...something went wrong.  Did you enter a strictly numerical number?');
     }
-    console.log(userName + ' answered ' + howManyBooks + ' on chance number ' + bookChance);
+
+    console.log('Question #6:\nI asked, "How many physical (not digital) books do you think I own?"\n' + userName + ' answered ' + howManyBooks + ' on chance number ' + bookChance);
+
     bookChance --;
+
     if (bookChance === 0) {
       alert('I\'m sorry, ' + userName + ', but you have used the alloted amount of guesses. I have 3,968 hard copy books.');
     }
   }
 }
 howManyBooks();
+
 //seventh question using an array and a loop
-//totally saved by Timea. THANKS!!!
 function whereDidILive(){
   var arrayOfStates = ['florida', 'nevada'];
   var chances = 6;
   var isCorrect = false;
+
   while (chances > 0) {
     if (isCorrect === true) {
       alert('Correct! I\'ve lived in a total of 3 states; Washington, Florida, and Nevada.');
-      console.log(userName + ' answered ' + whatStates + ' correctly.');
+      console.log('Question #7:\nI asked, "Please guess a state that I have lived in other than Washington."\n' + userName + ' answered ' + whatStates + ' correctly.');
       break;
     }
     var whatStates = prompt('Please guess a state other than Washington that I\'ve lived in. You have ' + chances + ' tries to get it correct.').toLowerCase();
@@ -132,16 +149,20 @@ function whereDidILive(){
         howManyCorrect ++;
       }
     }
+
     if (isCorrect === false){
-      alert('Sorry try again');
+      alert('That sounds like a nice place to visit, but I have never lived there. Please guess again.');
       chances --;
     }
+
     if (chances === 0) {
       alert('I\'m sorry, ' + userName + ', but you have used the alloted amount of guesses. I have lived in a total of 3 states: Washington, Florida, and Nevada.');
 
-      console.log(userName + ' has used the alloted amount of guesses. I have lived in a total of 3 states: Washington, Florida, and Nevada.');
+      console.log('Question #7:\n' + userName + ' has used the alloted amount of guesses. I have lived in a total of 3 states: Washington, Florida, and Nevada.');
     }
   }
 }
+
 whereDidILive();
-alert('Thank you for playing, ' + userName + ' You got ' + howManyCorrect + ' questions correct out of 7.');
+
+alert('Thank you for playing, ' + userName + '. You got ' + howManyCorrect + ' questions correct out of 7.');
